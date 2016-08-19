7d39a21d0ada3243ff1e149ecb3e6594f71e0b0d
hfegetude <hfegetude@gmail.com>
2016-08-19 12:23:41 +0200

    Added all buttons to GUI

--- CNC_gui.py
+++ CNC_gui.py
@@ -27,12 +27,69 @@ class Example(Frame):
         CenterFrame = Frame(self)
         CenterFrame.pack(fill = X)
         
-        CLabel = Label(CenterFrame, text = "C")
+        CLabel = Label(CenterFrame, text = "Center", width=6)
         CLabel.pack(side=LEFT, padx= 5, pady=5)
-        Xentry = Entry(CenterFrame, text = "X")
-        Xentry.pack(padx=5, expand=True)
-        Yentry = Entry(CenterFrame, text = "X")
-        Yentry.pack(padx=5, expand=True)
+        Xentry = Entry(CenterFrame, width=5)
+        Xentry.pack(side=LEFT, padx=5)
+        Xentry.insert(0, "X")
+        Yentry = Entry(CenterFrame, width=5)
+        Yentry.pack(side=LEFT, padx=5 )
+        Yentry.insert(0, "Y")
+
+        RadiusFrame = Frame(self)
+        RadiusFrame.pack(fill = X)
+
+        RLabel = Label(RadiusFrame, text="Radius", width=6)
+        RLabel.pack(side=LEFT, padx=5, pady=5)
+        Rentry = Entry(RadiusFrame, width=5)
+        Rentry.pack(side=LEFT, padx = 5)
+        Rentry.insert(0, "R")
+
+        AngleFrame = Frame(self)
+        AngleFrame.pack(fill = X)
+
+        ALabel = Label(AngleFrame, text="Angle", width=6)
+        ALabel.pack(side=LEFT, padx=5, pady=5)
+        A1Entry = Entry(AngleFrame, width=5)
+        A1Entry.pack(side=LEFT, padx=5)
+        A1Entry.insert(0, "From")
+        A2Entry = Entry(AngleFrame, width=5)
+        A2Entry.pack(side=LEFT, padx=5)
+        A2Entry.insert(0, "To")
+
+        LineFrame = Frame(self)
+        LineFrame.pack(fill=X)
+
+        LineButton = Button(LineFrame, text="Draw lline")
+        LineButton.pack(side=LEFT, padx=5, pady=9)
+
+        ToFrame = Frame(self)
+        ToFrame.pack(fill=X)
+        ToLabel = Label(ToFrame, text="To", width=6)
+        ToLabel.pack(side=LEFT, padx=5, pady=5)
+        ToXEntry = Entry(ToFrame, width=5)
+        ToXEntry.pack(side=LEFT, padx=5)
+        ToXEntry.insert(0, "X")
+        ToYEntry = Entry(ToFrame, width=5)
+        ToYEntry.pack(side=LEFT, padx=5)
+        ToYEntry.insert(0, "Y")
+
+        FromFrame = Frame(self)
+        FromFrame.pack(fill=X)
+        FromLabel = Label(FromFrame, text="From", width=6)
+        FromLabel.pack(side=LEFT, padx=5, pady=5)
+        FromXEntry = Entry(FromFrame, width=5)
+        FromXEntry.pack(side=LEFT, padx=5)
+        FromXEntry.insert(0, "X")
+        FromYEntry = Entry(FromFrame, width=5)
+        FromYEntry.pack(side=LEFT, padx=5)
+        FromYEntry.insert(0, "Y")
+
+        OutMessageFrame = Frame(self, borderwidth=1, relief=SUNKEN, width=15, height=5)
+        OutMessageFrame.pack(fill=X)
+        OutMessageLabel = Label(OutMessageFrame, width=15)
+        OutMessageLabel.pack(side=LEFT, padx=5)
+
 
 def main():
   
